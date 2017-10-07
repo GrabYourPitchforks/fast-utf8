00558a3d26d5cd070e9d0ddfcf02841ae0589dea
Levi Broderick <levib@microsoft.com>
2017-10-06 21:16:36 -0700

    Commit project skeleton

---
+++ FastUtf8Tester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FastUtf8Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
