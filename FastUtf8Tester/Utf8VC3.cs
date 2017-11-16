5c3f5bacf590d836bb54b10a0da56ccaf83a27fa
Levi Broderick <levib@microsoft.com>
2017-11-15 17:33:48 -0800

    New experimental implementation of byte-by-byte consumption logic

--- FastUtf8Tester/Utf8VC3.cs
+++ FastUtf8Tester/Utf8VC3.cs
@@ -10,10 +10,22 @@ namespace System.Buffers.Text
     public struct Utf8VC3
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static uint ROL8(byte value)
+        {
+            return (uint)((value >> 4) | (value << 4));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static uint ROL32(uint value, int shift)
         {
             // shift &= 0x1F;
             return (value << shift) | (value >> (32 - shift));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static uint ROR32(uint value, int shift)
+        {
+            return (value >> shift) | (value << (32 - shift));
         }
 
         public Status Foo(byte codePoint)
@@ -25,6 +37,78 @@ namespace System.Buffers.Text
             Done = 0,
             Incomplete = 1,
             Invalid = 2
+        }
+
+        public static uint Foo3AndUpdateState(uint codePoint, uint oldState)
+        {
+            // The state value is [ w0000000 zzzzzzzz yyyyyyyy xxxxxxxx ],
+            // where zzzzzzzz = number of continuation bytes left to read,
+            //       yyyyyyyy = the lower bound (inclusive) of the next valid continuation byte,
+            //       xxxxxxxx = the range [upper bound - lower bound] (inclusive) of the next valid continuation byte,
+            // and   w is set if there was an error.
+
+            // We only care about zzzzzzzz for the purpose of determining whether we're
+            // expecting to read the start of a sequence or a continuation code unit.
+
+            if ((oldState & 0xFF0000U) == 0)
+            {
+                // If this code unit is [ 00..7F ], then it's an ASCII character,
+                // which is a valid single-unit sequence, so we're done.
+
+                if ((codePoint & 0x80) == 0) { return 0; }
+
+                // If this code unit is [ C2..DF ], then it's the start of a 2-byte sequence.
+                // We expect one more continuation byte, and its valid range is [ 80..BF ].
+
+                codePoint -= 0xC2U;
+                if (codePoint <= (0xDFU - 0xC2U)) { return 0x1803F; }
+
+                // If this code unit is [ E0..EF ], then it's the start of a 3-byte sequence.
+                // We expect two more continuation bytes, but the valid range of the first
+                // continuation byte depends on the value of the first byte of the sequence.
+
+                codePoint -= (0xE0U - 0xC2U);
+                if (codePoint <= (0xEFU - 0xE0U))
+                {
+                    return ((1U >> (int)codePoint) << 13) // 0x2000 iff code point is E0
+                        + ((0x1BFFC0U >> (int)codePoint) & 0x20U) // 0x20 iff code point is *not* E0 or ED, 
+                        + 0x20000U // 2 continuation bytes expected
+                        + 0x8000U // lower bound (inclusive) is 80 (or A0 iff code point is E0)
+                        + 0x1FU; // range (inclusive) is 1F iff code point is E0 or ED, otherwise 3F
+                }
+
+                // If this code unit is [ F0..F4 ], then it's the start of a 4-byte sequence.
+                // We expect three more continuation bytes, but the valid range of the first
+                // continuation byte depends on the value of the first byte in the sequence.
+
+                codePoint -= (0xF0U - 0xE0U);
+                if (codePoint <= (0xF4U - 0xF0U))
+                {
+                    return ((1U >> (int)codePoint) << 13) // 0x2000 iff code point is F0
+                        + ((0x33320U >> (4 * (int)codePoint)) & 0xF0U) // 0x20 iff code point is F0, 0x30 iff code point is F1..F3, 0x00 iff code point is F4
+                        + 0x30000U // 3 continuation bytes expected
+                        + 0x8000U // lower bound (inclusive) is 80 (or 90 iff code point is F0)
+                        + 0xFU; // range (inclusive) is 0F, 2F, or 3F, depending on first byte
+                }
+            }
+            else
+            {
+                // We're expecting a continuation byte.
+
+                if ((codePoint - ((uint)(ushort)oldState >> 8)) <= (byte)oldState)
+                {
+                    // Incoming continuation byte was in the valid range.
+                    // Decrement zzzzzzzz by 1 and reset yyyyyyyyxxxxxxxx to 803F.
+                    // The code below is written such that this becomes a single AND and LEA.
+
+                    return (oldState & 0x30000U) + 0x803FU - 0x10000U;
+                }
+            }
+
+            // If we reached this point, an invalid code unit was seen.
+            // Report an error (w bit set) to the caller.
+
+            return (uint)1 << 31;
         }
 
         private static Status Foo2(uint codePoint, uint oldData, out uint newData)
